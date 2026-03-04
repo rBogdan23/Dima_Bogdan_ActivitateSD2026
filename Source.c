90b752476a46356a47f5d8297f94cb3b5935a099
bogdan <bogdan.k983@gmail.com>
2026-03-04 14:19:00 +0200

    am introdus functia de citire a unui nr intreg

--- Source.c
+++ Source.c
@@ -1,6 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 
 int main() {
 	printf("Hello world! \n");
+	int variabila = 0;
+	scanf("%d", &variabila);
+	printf("Ai introdus: %d", variabila);
 	return 0;
 }
