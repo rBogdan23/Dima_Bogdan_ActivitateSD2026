beefd5ec1d1f6b50d738b7d16a193b745b3ee0fb
bogdan <bogdan.k983@gmail.com>
2026-03-11 11:37:45 +0200

    structura telefon

---
+++ Lab01.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+struct Telefon {
+	int id;
+	int RAM;
+	char* producator;
+	float pret;
+	char serie;
+};
--- Source.c
+++ Source.c
@@ -1,11 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-
-int main() {
-	printf("Hello world! \n");
-	int variabila = 0;
-	scanf("%d", &variabila);
-	printf("Ai introdus: %d", variabila);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//
+//int main() {
+//	printf("Hello world! \n");
+//	int variabila = 0;
+//	scanf("%d", &variabila);
+//	printf("Ai introdus: %d", variabila);
+//	// citirea unei variabile
+//	return 0;
+//}
